1942812f672967a4ff93def051377b5fbeee1ba0
K.ShivaNagendra <shivaaman751@gmail.com>
2023-12-28 09:15:46 -0500

    Adding Collections: Lists

--- src/Collections.kt
+++ src/Collections.kt
@@ -1,3 +1,20 @@
 fun main(){
-    
+// Collections:
+    //Lists
+    //Sets
+    //Maps
+// Lists:
+    // you can duplicate but you cant change immutable
+    val cars = listOf("Honda","Lamborghini","Pagani","BMW")
+    println(cars)
+
+    // you can change the items , this can mutable
+    val fruits = mutableListOf("Apple", "Mango","grapes")
+    println(fruits)
+
+    // you can do casting , casting means : you can change object from one datatype to other type
+    val newcars: List<String> = fruits
+    println(newcars)
+
+
 }
