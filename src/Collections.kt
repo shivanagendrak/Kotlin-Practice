e06dcaa71dd76cc4a5032746f544359d9f81aba7
K.ShivaNagendra <shivaaman751@gmail.com>
2023-12-28 09:36:01 -0500

    Updating Lists with some builtin-functions for list

--- src/Collections.kt
+++ src/Collections.kt
@@ -12,9 +12,26 @@ fun main(){
     val fruits = mutableListOf("Apple", "Mango","grapes")
     println(fruits)
 
-    // you can do casting , casting means : you can change object from one datatype to other type
+    // you can do "CASTING" , casting means : you can change object from one datatype to other type
     val newcars: List<String> = fruits
     println(newcars)
 
+    // Accessing Items in List
+    println("i love ${fruits[0]} in the fruits")
+    // when i want to see which item is first in the list
+    println("The first Item from the list of fruits is ${fruits.first()}")
+// when i want to see which item is last in the list
+    println("The Last Item from the list of fruits is ${fruits.last()}")
+    // i want to calculate how many types of fruits are in the fruits list
+    println("Total types of Fruits in the fruits list are ${fruits.count()}")
+// to check the item is there in the list or not we are using "in" keyword
+    // true or false
+    println("Apple" in fruits)
 
+    // if you want to add or remove something from a list
+    fruits.add("Pineapple")
+    println("Adding a fruits to list $fruits")
+    // removing pineapple from list
+    fruits.remove("Pineapple")
+    println(fruits)
 }
